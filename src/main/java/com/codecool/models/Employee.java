c570330198800b1236b2b54d18b96571ed98a316
feshout <maciej.ukowski@gmail.com>
2018-03-06 15:50:00 +0100

    Add employee model

---
+++ src/main/java/com/codecool/models/Employee.java
@@ -0,0 +1,54 @@
+package com.codecool.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Employee implements Serializable {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private BigDecimal incomes;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String email, BigDecimal incomes) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.incomes = incomes;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public BigDecimal getIncomes() {
+        return incomes;
+    }
+
+    public void setIncomes(BigDecimal incomes) {
+        this.incomes = incomes;
+    }
+}
