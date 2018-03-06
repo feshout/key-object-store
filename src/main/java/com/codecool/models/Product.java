f43901d830e75f0b3dc55b85a3e177ab621bae06
feshout <maciej.ukowski@gmail.com>
2018-03-06 15:50:14 +0100

    Add product model

--- src/main/java/com/codecool/models/Product.java
+++ src/main/java/com/codecool/models/Product.java
@@ -1,4 +1,20 @@
 package com.codecool.models;
 
-public class Product {
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Product implements Serializable {
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Product(String name, String description, BigDecimal price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
 }
