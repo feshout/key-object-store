b32de6686a0cfbc9b376d2c78cdfab3669a470c0
feshout <maciej.ukowski@gmail.com>
2018-03-06 15:51:05 +0100

    Temporary main

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -1,13 +1,23 @@
 package com.codecool;
 
-/**
- * Hello world!
- *
- */
+import com.codecool.models.Employee;
+import com.codecool.models.Product;
+import com.codecool.utils.DatabaseConnection;
+
+import java.math.BigDecimal;
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        Employee employee1 = new Employee("Marian", "Koster", "koster@gmail.com", new BigDecimal("2000.0"));
+        Employee employee2 = new Employee("Adrian", "Marny", "marny@gmail.com", new BigDecimal("2000.0"));
+
+        Product product1 = new Product("Kiełbasa", "Kiełba z pierzem", new BigDecimal("15.00"));
+        Product product2 = new Product("Masło", "Masło 100%", new BigDecimal("5.00"));
+
+        DatabaseConnection databaseConnection = new DatabaseConnection("192.168.10.195", 9000);
+
     }
 }
